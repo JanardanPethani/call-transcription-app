7e4ba27c8a97d69709d41a45ac4474217e73a073
JanardanPethani <jpethani11@gmail.com>
2024-10-08 21:39:40 +0530

    Add fixed adsense scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <GoogleAdsense />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9821198488968811"
+          crossorigin="anonymous"
+        ></script>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-9821198488968811"
+        ></meta>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
